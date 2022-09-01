1a11e82569ad9e092baf96d5e5405ed76efe7dfa
jykang217 <112591879+jykang217@users.noreply.github.com>
2022-09-01 14:53:49 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -10,4 +10,9 @@ def test_it():
 	Write a simple test.
 	'''
 
-	raise NotImplementedError('Need to implement a simple test.')
+	s = Stack()
+	assert s.size() == 0
+	s.push(1)
+	assert s.size() == 1
+	
+		
